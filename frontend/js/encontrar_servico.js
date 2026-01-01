1c3eea25da5ac761a19d16e091a5a1dc14a20cab
Edmilson Junior Jose <134197625+Edmilson-Junior@users.noreply.github.com>
2026-01-01 12:03:46 +0100

    nova conclusão
    nova conclusão

---
+++ frontend/js/encontrar_servico.js
@@ -0,0 +1,21 @@
+// codigo javaScript para carregar os servicos dinamicamente, mas de acordo com a pesquisa feita no index.html
+
+var texto = document.getElementById("input-pesquisa").value;
+
+
+
+var servicos = [
+    { nome: "", descricao: "Criação de sites responsivos e modernos.", preco: 1500 },
+    { nome: "Design Gráfico", descricao: "Criação de logotipos, banners e materiais promocionais.", preco: 800 },
+    { nome: "Marketing Digital", descricao: "Estratégias de marketing para aumentar sua presença online.", preco: 1200 },
+    { nome: "Redação e Conteúdo", descricao: "Produção de artigos, blogs e conteúdo para redes sociais.", preco: 500 }
+];
+
+
+
+
+
+
+
+
+
